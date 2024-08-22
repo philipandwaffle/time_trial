1ab9d711d2be6c7833a85e5bf717dbc0de12bdc2
Philip Cox <philipcox197@gmail.com>
2024-08-23 00:03:37 +0100

    logic graphs

--- src/level/logic_tree_v2.rs
+++ src/level/logic_tree_v2.rs
@@ -4,21 +4,69 @@ pub struct LogicGraph {
     nodes: Vec<Node>,
 }
 impl LogicGraph {
+    pub fn new(input_nodes: Vec<usize>, output_nodes: Vec<usize>, nodes: Vec<Node>) -> Self {
+        return Self {
+            input_nodes,
+            output_nodes,
+            nodes,
+        };
+    }
+
     pub fn process(&mut self, input: Vec<bool>) -> Vec<bool> {
-        if input.len() != self.output_nodes.len() {
+        if input.len() != self.input_nodes.len() {
             panic!();
         }
         for i in 0..input.len() {
             self.nodes[self.input_nodes[i]].state = input[i];
         }
 
-        return vec![];
+        let num_out = self.output_nodes.len();
+        let mut output = Vec::with_capacity(num_out);
+        for i in 0..num_out {
+            let in_i = self.output_nodes[i];
+            self.update_node(in_i);
+            output.push(self.nodes[in_i].state);
+        }
+
+        self.reset_updated();
+        return output;
     }
 
     fn update_node(&mut self, i: usize) {
         let children = self.nodes[i].in_nodes.clone();
-        for child_i in children {
-            self.update_node(child_i);
+
+        for child_i in children.iter() {
+            self.update_node(*child_i);
+        }
+
+        if self.nodes[i].updated {
+            return;
+        }
+
+        self.nodes[i].state = match self.nodes[i].op {
+            Operator::None => return,
+            Operator::And => {
+                let mut s = true;
+                for child_i in children {
+                    s = s && self.nodes[child_i].state;
+                }
+                s
+            }
+            Operator::Or => {
+                let mut s = false;
+                for child_i in children {
+                    s = s || self.nodes[child_i].state;
+                }
+                s
+            }
+        };
+
+        self.nodes[i].updated = true;
+    }
+
+    fn reset_updated(&mut self) {
+        for node in self.nodes.iter_mut() {
+            node.updated = false;
         }
     }
 }
@@ -40,7 +88,8 @@ impl Node {
     }
 }
 
-enum Operator {
+pub enum Operator {
+    None,
     And,
     Or,
 }
--- src/level/mod.rs
+++ src/level/mod.rs
@@ -10,3 +10,52 @@ mod logic_tree_v2;
 pub mod manager;
 mod output;
 mod time_shift;
+
+#[cfg(test)]
+mod tests {
+    use super::logic_tree_v2::{LogicGraph, Node, Operator};
+
+    #[test]
+    fn or_logic_graph() {
+        let nodes = vec![
+            Node::new(Operator::None, vec![]),
+            Node::new(Operator::None, vec![]),
+            Node::new(Operator::Or, vec![0, 1]),
+        ];
+
+        let mut logic_graph = LogicGraph::new(vec![0, 1], vec![2], nodes);
+        let out = logic_graph.process(vec![true, true]);
+        assert!(out[0] == true);
+
+        let out = logic_graph.process(vec![true, false]);
+        assert!(out[0] == true);
+
+        let out = logic_graph.process(vec![false, true]);
+        assert!(out[0] == true);
+
+        let out = logic_graph.process(vec![false, false]);
+        assert!(out[0] == false);
+    }
+
+    #[test]
+    fn and_logic_graph() {
+        let nodes = vec![
+            Node::new(Operator::None, vec![]),
+            Node::new(Operator::None, vec![]),
+            Node::new(Operator::And, vec![0, 1]),
+        ];
+
+        let mut logic_graph = LogicGraph::new(vec![0, 1], vec![2], nodes);
+        let out = logic_graph.process(vec![true, true]);
+        assert!(out[0] == true);
+
+        let out = logic_graph.process(vec![true, false]);
+        assert!(out[0] == false);
+
+        let out = logic_graph.process(vec![false, true]);
+        assert!(out[0] == false);
+
+        let out = logic_graph.process(vec![false, false]);
+        assert!(out[0] == false);
+    }
+}
